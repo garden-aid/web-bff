72b77bf01d602bd85d483d3f0f79482980547897
John McKim <johncmckim@gmail.com>
2016-09-11 08:55:37 +1000

    Fix issue with auth lambda and add tests

--- src/auth/utils.js
+++ src/auth/utils.js
@@ -1,18 +1,23 @@
 
 module.exports.getAuthInfo = (methodArn) => {
+  if (!methodArn) return null;
+
   const tmp = methodArn.split(':');
   const apiGatewayArnTmp = tmp[5].split('/');
 
   return {
     accountId: tmp[4],
     region: tmp[3],
-    stage: apiGatewayArnTmp[1],
     restApiId: apiGatewayArnTmp[0],
+    stage: apiGatewayArnTmp[1],
+    method: apiGatewayArnTmp[2],
   };
 };
 
 
 module.exports.getToken = (authorizationToken) => {
+  if (!authorizationToken) return null;
+
   const tokenParts = authorizationToken.split(' ');
-  return tokenParts[2];
+  return tokenParts[1];
 };
--- tests/all.js
+++ tests/all.js
@@ -2,4 +2,5 @@
 process.env.NODE_ENV = 'test';
 
 require('./env');
+require('./auth/all');
 require('./services/all');
